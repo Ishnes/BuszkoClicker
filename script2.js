983cd64301d26a227a59d4903398e76b7c26fac4
Ishnes <marcelciesiolkiewicz210@gmail.com>
2024-12-16 19:47:21 +0100

    Add files via upload

--- script2.js
+++ script2.js
@@ -14,13 +14,16 @@ document.getElementById('songsTab').addEventListener('click', function() {
     showSection('songsSection');
 });
 
+document.getElementById('LeaderboardTab').addEventListener('click', function() {
+    showSection('leaderboardSection');
+});
+
 function showSection(sectionId) {
     // Ukryj wszystkie sekcje
     const sections = document.querySelectorAll('.tab-section');
     sections.forEach(section => {
         section.classList.remove('active');
     });
-
     // Pokaż wybraną sekcję
     const section = document.getElementById(sectionId);
     section.classList.add('active');
