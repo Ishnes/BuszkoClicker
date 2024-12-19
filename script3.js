3d0a918920557fa62a9d2ddafb833df53940d88a
Ishnes <marcelciesiolkiewicz210@gmail.com>
2024-12-19 19:10:23 +0100

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,12 +1,36 @@
+const instrukcja = document.getElementById("instrukcja");
+
 function Iopen()
 {
-	document.getElementById("instrukcja").style.display="block";
-}
-
+	instrukcja.style.display="block";
+	
 function Iclose()
 {
-	document.getElementById("instrukcja").style.display="none";
+	instrukcja.style.display="none";
+	
+const neon = document.getElementById("neon");
+
+function lightoff()
+{
+	neon.style.textShadow = "none";
+	
 }
 
+function lighton()
+{
+	neon.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #faf, 0 0 80px #faf, 0 0 90px #faf, 0 0 100px #faf";
+}
 
+function neontoggle()
+{
+	if(neon.style.textShadow  ===  "none")
+	{
+		lighton();
+	}else
+	{
+		lightoff();
+	}
+	
+}
 
+neon.addEventListener("click", neontoggle)
