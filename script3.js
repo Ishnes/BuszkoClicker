3036380019336bbe428ca886bb9a040206a6ebfd
Ishnes <marcelciesiolkiewicz210@gmail.com>
2024-12-22 18:12:07 +0100

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,36 +1,30 @@
 const instrukcja = document.getElementById("instrukcja");
-
-function Iopen()
-{
-	instrukcja.style.display="block";
-	
-function Iclose()
-{
-	instrukcja.style.display="none";
-}
 const neon = document.getElementById("neon");
 
-function lightoff()
-{
-	neon.style.textShadow = "none";
-	
+function Iopen() {
+    instrukcja.style.display = "block";
 }
 
-function lighton()
-{
-	neon.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #faf, 0 0 80px #faf, 0 0 90px #faf, 0 0 100px #faf";
+function Iclose() {
+    instrukcja.style.display = "none";
 }
 
-function neontoggle()
-{
-	if(neon.style.textShadow  ===  "none")
-	{
-		lighton();
-	}else
-	{
-		lightoff();
-	}
-	
+function lightoff() {
+    neon.style.textShadow = "none";
 }
 
-neon.addEventListener("click", neontoggle);
+function lighton() {
+    neon.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #faf, 0 0 80px #faf, 0 0 90px #faf, 0 0 100px #faf";
+}
+
+function neontoggle() {
+    if (neon.style.textShadow === "none") {
+        lighton();
+    } else {
+        lightoff();
+    }
+}
+
+
+
+    neon.addEventListener("click", neontoggle);
